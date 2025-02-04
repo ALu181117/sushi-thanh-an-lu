48e335239d3b692a7e54f9ab890088ef32f2dd6f
Dmitry <dzinoviev@gmail.com>
2025-02-04 00:23:53 -0500

    Ready for HW2

--- Sushi.cc
+++ Sushi.cc
@@ -1,99 +1,74 @@
-#include "Sushi.hh"
 #include <iostream>
-#include <cctype>
 #include <fstream>
-#include <sstream>
+#include <algorithm>
+#include <iomanip>
+#include <cstdio>
+#include "Sushi.hh"
 
-const size_t Sushi::MAX_INPUT = 256;
-const size_t Sushi::HISTORY_LENGTH = 10;
-const std::string Sushi::DEFAULT_PROMPT = "sushi> ";
-
-std::string Sushi::read_line(std::istream &in){
+std::string Sushi::read_line(std::istream &in)
+{
   std::string line;
-  char c;
-  size_t count = 0;
-
-  // DZ: Very inefficient; Why not use std::getline? 
-  while (in.get(c)){
-	  if (c == '\n') break;
-	  line +=c;
-	  count++;
+  if(!std::getline (in, line)) {// Has the operation failed?
+    if(!in.eof()) { 
+      std::perror("getline");
+    }
+    return "";
+  }
+    
+  // Is the line empty?
+  if(std::all_of(line.begin(), line.end(), isspace)) {
+    return "";
   }
 
-  if (in.fail() && !in.eof()){
-	  // Handle I/O errors
-	  std::perror("Error when reading input!");
-	  return "";
-  }
-
-  // DZ: This is incorrect. The line has been already read in full.
-  //Handle lines exceeds max input
-  // DZ: No need to call a function
-  if (count > MAX_INPUT){
-  //if (count > Sushi::getMAXINPUT()){
-	  while (in.get(c) && c != '\n');
-	  std::cerr << "Line too long, truncated." << std::endl;
-  return "";
+  // Is the line too long?
+  if(line.size() > MAX_INPUT_SIZE) {
+    line.resize(MAX_INPUT_SIZE);
+    std::cerr << "Line too long, truncated." << std::endl;
   }
   
-  //Check if the line is empty or contains only whitespace
-  for (char ch : line) {
-    if (!std::isspace(static_cast<unsigned char>(ch))) {
-      return line;
+  return line; 
+}
+
+bool Sushi::read_config(const char *fname, bool ok_if_missing)
+{
+  // Try to open a config file
+  std::ifstream config_file(fname);
+  if (!config_file) {
+    if (!ok_if_missing) {
+      std::perror(fname);
+      return false;
     }
+    return true;
   }
 
-  return "";
+  // Read the config file
+  while(!config_file.eof()) {
+    std::string line = read_line(config_file);
+    store_to_history(line);
+  }
+  
+  return true; 
 }
 
-bool Sushi::read_config(const char *fname, bool ok_if_missing){
-	std::ifstream file(fname);
+void Sushi::store_to_history(std::string line)
+{
+  if (line.empty()) {
+    return;    
+  }
 
-	if(!file && !ok_if_missing){
-	  // DZ: Wrong use of perror
-	  //std::perror("Error opening file");
-	  std::perror(fname);
-		return false;
-	}
-	if (!file && ok_if_missing) return true;
-	std::string line;
-	while(true){
-		line = read_line(file);
-		if (line.empty()) {
-		  // DZ: wrong condition, stops the loop at the first blank line
-		  break;
-		}
-		// DZ: This does not belong here
-		store_to_history(line);
-	}
-	if (file.bad()){
-	  // DZ: See above
-		std::perror("Error reading file");
-		return false;
-	}
-	// DZ: C++ closes local ifstream automatically
-	// file.close();
-	return true;
+  // Is the history buffer full?
+  while (history.size() >= HISTORY_LENGTH) {
+    history.pop_front();
+  }
+  
+  history.emplace_back(line);
 }
 
-void Sushi::store_to_history(std::string line){
-	if (line.empty()) return; // Check nullptr or empty 
-	history.insert(history.begin(),line); // Insert the new line to the beginning
-	if (history.size() > HISTORY_LENGTH) { // Check if the history size is exceeds the HISTORY_LENGTH
-		history.pop_back(); // Discard the oldest entry
-	}
-
-}
-
-void Sushi::show_history(){
-  for (size_t i = 0; i<history.size(); ++i){
-    // DZ: You do not need std::ostringstream for this
-	  std::ostringstream index_stream;
-	  index_stream.width(5);
-	  index_stream.fill(' ');
-	  index_stream << (i+1);
-	  // DZ: Must be TWO spaces
-	  std::cout << index_stream.str() << " "<<history[i]<<std::endl;
+void Sushi::show_history() const
+{
+  int index = 1;
+  for (const auto &cmd: history) {
+    std::cout << std::setw(5) << index++ << "  " << cmd << std::endl;
   }
 }
 
--- Sushi.hh
+++ Sushi.hh
@@ -1,13 +1,14 @@
 #pragma once
 #include <iostream> // New include
 #include <string> 
-#include <vector>
+#include <deque>
 
 class Sushi {
 private:
-  std::vector<std::string> history; // Change history type to vector
-  static const size_t HISTORY_LENGTH;
-  static const size_t MAX_INPUT;
+  std::deque<std::string> history; 
+  static const size_t HISTORY_LENGTH = 10;
+  static const size_t MAX_INPUT_SIZE = 16;
+  bool exit_flag = false; // New attribute
 
 public:
   Sushi() : history() {};
@@ -16,20 +17,17 @@ public:
   static std::string *getenv(const char *name); // New method
   bool read_config(const char *fname, bool ok_if_missing);
   void store_to_history(std::string line);
-  void show_history();
+  void show_history() const;
   void re_parse(int i); // New method
   void set_exit_flag(); // New method
   bool get_exit_flag() const; // New method
   static int parse_command(const std::string command); // New method
-  // DZ: No need for this function
-  /*
-  static size_t getMAXINPUT(){
-	return MAX_INPUT;
-  }
-  */
+  
   static const std::string DEFAULT_PROMPT;
+  static const std::string DEFAULT_CONFIG;
 };
 
 #define UNUSED(expr) do {(void)(expr);} while (0)
+
 // New declaration
 extern Sushi my_shell;
